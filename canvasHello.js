40fa465fb3cf1af4b292574e34a1d333fddcf37f
bidangol <bikram.dangol@ymail.com>
2022-04-18 18:20:20 -0400

    green and image

---
+++ canvasHello.js
@@ -0,0 +1,7 @@
+// var c = document.getElementById("lineCanvas");
+// var ctx = c.getContext("2d");
+// var height = 50
+// c.setAttribute('height', height)
+// ctx.moveTo(100,0);
+// ctx.lineTo(100,height);
+// ctx.stroke();
