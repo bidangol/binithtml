9bdd98b7752dd0a913e95ea53c5d7c3ef18108b2
bidangol <bikram.dangol@ymail.com>
2022-05-13 12:31:29 -0400

    1 file modified

--- canvasHello.js
+++ canvasHello.js
@@ -36,13 +36,70 @@ const canvasRed = document.getElementById("redDeath");
 const ctxRed = canvasRed.getContext("2d");
    
 ctxRed.beginPath();
-ctxRed.bezierCurveTo(100, 334, 210, 290, 195, 90, 250, 100); ctxRed.stroke();
+ctxRed.moveTo(100, 334);
+ctxRed.bezierCurveTo( 210, 334, 100, 90, 250, 100); ctxRed.stroke();
+ctxRed.closePath();
 
 
+const canvasHuge = document.getElementById("HugeDeath");
+const ctxHuge = canvasHuge.getContext("2d");
+var y = 320;
+ctxHuge.beginPath();
+//ctxHuge.moveTo(20, 0);
+ctxHuge.arc(100, y+75, 50, 0, 2*Math.PI);
+ctxHuge.stroke();
 
-const canvasdragon = document.getElementById("imaginationDragons");
-const ctxdragon = canvasRed.getContext("2d");
+ctxHuge.beginPath();
+//ctxHuge.moveTo(20, 0);
+ctxHuge.arc(218, y+45, 40, 0, 2*Math.PI);
+ctxHuge.stroke();
    
-ctxRed.beginPath();
-ctxRed.bezierCurveTo(450, 334, 210, 990, 195, 200, 590, 100); ctxRed.stroke();
+// third biggest eye
+ctxHuge.beginPath();
+//ctxHuge.moveTo(20, 0);
+ctxHuge.arc(315, y+20, 32, 0, 2*Math.PI);
+ctxHuge.stroke();
+
+// smallest eye
+ctxHuge.beginPath();
+//ctxHuge.moveTo(20, 0);
+ctxHuge.arc(450, y+20, 15, 0, 2*Math.PI);
+ctxHuge.stroke();
+
+// nose
+ctxHuge.beginPath();
+ctxHuge.ellipse(70, y+175, 22, 40, 0.2, 0, 2*Math.PI);
+ctxHuge.stroke();
+
+// eye in
+ctxHuge.beginPath();
+ctxHuge.ellipse(64, y+86, 2, 22, 0.2, 0, 2*Math.PI);
+ctxHuge.stroke();
+ctx.fillStyle = "#FF0000";
+
+// second biggest eye
+ctxHuge.beginPath();
+//ctxHuge.moveTo(20, 0);
+ctxHuge.arc(391, y+0, 24, 0, 2*Math.PI);
+ctxHuge.stroke();
+
+// mouth
+ctxHuge.beginPath();
+ctxHuge.moveTo(30, y+235);
+ctxHuge.bezierCurveTo(410, y+200, 10, y+592, 410, y+592);
+ctxHuge.stroke();
+
+//  front head
+ctxHuge.beginPath();
+//ctxHuge.moveTo(10, y-100);
+ctxHuge.bezierCurveTo(10, y-48, 0, y+133, 130, y+655);
+ctxHuge.stroke();
+
+// top head
+ctxHuge.beginPath();
+ctxHuge.moveTo(10, y-48);
+ctxHuge.bezierCurveTo(-20, y-200, 300, y- 153, 430, y- 148);
+ctxHuge.stroke();
+
+//ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle)
 
